3f2c68e4f92989a47788cf5837b770f73580e38f
Janos Pasztor <janos@pasztor.at>
2016-11-08 11:17:54 +0100

    Added support for cached packages list

--- src/CDEDisloClient.php
+++ src/CDEDisloClient.php
@@ -4,10 +4,13 @@ namespace Ixolit\Dislo\CDE;
 
 use Ixolit\Dislo\CDE\Exceptions\KVSKeyNotFoundException;
 use Ixolit\Dislo\Client;
+use Ixolit\Dislo\Response\PackagesListResponse;
+use Ixolit\Dislo\WorkingObjects\Package;
 
 class CDEDisloClient extends Client {
 	/**
-	 *
+	 * Retrieve a list of all packages registered in the system. This version uses the CDE key-value store to make
+	 * package list ist retrieval faster.
 	 *
 	 * @param null|string $serviceIdentifier
 	 * @param bool        $cached Use key-value store cached packages list instead of fetching it from the API.
@@ -21,14 +24,11 @@ class CDEDisloClient extends Client {
 		if (!$cached) {
 			return parent::packagesList($serviceIdentifier);
 		} else {
-			//todo add KVS shortcut support once Dislo supports storing packages in the new format.
-			/*try {
-				$kvsPackageData = CDE::getKVSAPI()->get('packages');
-				var_dump($kvsPackageData);
-				exit;
-			} catch (KVSKeyNotFoundException $e) {*/
+			try {
+				return PackagesListResponse::fromResponse(CDE::getKVSAPI()->get('apiPackages'));
+			} catch (KVSKeyNotFoundException $e) {
 				return parent::packagesList($serviceIdentifier);
-			//}
+			}
 		}
 	}
 }
