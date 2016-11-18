2c3ab18dd245a9a72afc9a1469f33da40c359fe8
Janos Pasztor <janos@pasztor.at>
2016-11-18 12:37:25 +0100

    Added packageGet

--- src/CDEDisloClient.php
+++ src/CDEDisloClient.php
@@ -4,10 +4,33 @@ namespace Ixolit\Dislo\CDE;
 
 use Ixolit\Dislo\CDE\Exceptions\KVSKeyNotFoundException;
 use Ixolit\Dislo\Client;
+use Ixolit\Dislo\Exceptions\ObjectNotFoundException;
+use Ixolit\Dislo\Response\PackageGetResponse;
 use Ixolit\Dislo\Response\PackagesListResponse;
 use Ixolit\Dislo\WorkingObjects\Package;
 
 class CDEDisloClient extends Client {
+	/**
+	 * @param string $packageIdentifier
+	 *
+	 * @param bool   $cached
+	 *
+	 * @return PackageGetResponse
+	 * @throws ObjectNotFoundException
+	 */
+	public function packageGet(
+		$packageIdentifier,
+		$cached = true
+	) {
+		$packages = $this->packagesList(null, $cached)->getPackages();
+		foreach ($packages as $package) {
+			if ($package->getPackageIdentifier() == $packageIdentifier) {
+				return new PackageGetResponse($package);
+			}
+		}
+		throw new ObjectNotFoundException('package with ID ' . $packageIdentifier);
+	}
+
 	/**
 	 * Retrieve a list of all packages registered in the system. This version uses the CDE key-value store to make
 	 * package list ist retrieval faster.
