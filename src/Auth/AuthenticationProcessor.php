9742c6c835b43f2c8cb154206568d4e58d62c57f
Markus Marchewa <m.marchewa@ixolit.com>
2017-02-28 15:36:13 +0100

    authentication processor: permanent vs volatile mode

--- src/Auth/AuthenticationProcessor.php
+++ src/Auth/AuthenticationProcessor.php
@@ -17,6 +17,11 @@ class AuthenticationProcessor {
 
 	const COOKIE_NAME_AUTH_TOKEN = 'auth-token';
 
+	const KEY_VOLATILE = 'volatile';
+
+	const TOKEN_TIMEOUT_VOLATILE =  1 * 24*60*60;   //  1 day
+	const TOKEN_TIMEOUT_LONGTERM = 90 * 24*60*60;   // 90 days, dislo maximum
+
 	/**
 	 * @var RequestAPI
 	 */
@@ -26,27 +31,53 @@ class AuthenticationProcessor {
 	 * @var ResponseAPI
 	 */
 	private $responseApi;
+
+	/**
+	 * @var string
+	 */
+	private $cookieName;
+
 	/**
 	 * @var int
 	 */
-	private $tokenTimeout;
+	private $tokenTimeoutLongterm;
 
 	/**
+	 * @var int
+	 */
+	private $tokenTimeoutVolatile;
+
+	/**
+	 * Helper for user authentication and verification. Stores the authentication token in a cookie.
+	 * The default mode uses the maximum token lifetime and renews the cookie frequently ("stay logged in").
+	 * The alternative volatile mode uses a short token lifetime and a session cookie.
+	 *
 	 * @param RequestAPI  $requestApi
 	 * @param ResponseAPI $responseApi
-	 * @param int         $tokenTimeout
+	 * @param int         $tokenTimeoutLongterm
+	 * @param int         $tokenTimeoutVolatile
+	 * @param string      $cookieName
 	 */
-	public function __construct(RequestAPI $requestApi, ResponseAPI $responseApi, $tokenTimeout = 2592000) {
+	public function __construct(
+		RequestAPI $requestApi,
+		ResponseAPI $responseApi,
+		$tokenTimeoutLongterm = self::TOKEN_TIMEOUT_LONGTERM,
+		$tokenTimeoutVolatile = self::TOKEN_TIMEOUT_VOLATILE,
+		$cookieName = self::COOKIE_NAME_AUTH_TOKEN
+	) {
 		$this->requestApi  = $requestApi;
 		$this->responseApi = $responseApi;
-		$this->tokenTimeout = $tokenTimeout;
+		$this->tokenTimeoutLongterm = $tokenTimeoutLongterm;
+		$this->tokenTimeoutVolatile = $tokenTimeoutVolatile;
+		$this->cookieName = $cookieName;
 	}
 
 	/**
-	 * Authenticate a user. If successful, the authentication token is set into a cookie, and also returned.
+	 * Authenticate a user. If successful, the authentication token is set into a cookie and also returned.
 	 *
 	 * @param string $uniqueUserField
 	 * @param string $password
+	 * @param bool $volatile If true, use short token lifetime on server and create session cookie on client
 	 *
 	 * @return string
 	 *
@@ -54,19 +85,19 @@ class AuthenticationProcessor {
 	 * @throws AuthenticationInvalidCredentialsException
 	 * @throws AuthenticationException
 	 */
-	public function authenticate($uniqueUserField, $password) {
+	public function authenticate($uniqueUserField, $password, $volatile = false) {
 		$apiClient = new Client();
 		$authenticationResponse = $apiClient->userAuthenticate(
 			$uniqueUserField,
 			$password,
 			$this->requestApi->getRemoteAddress()->__toString(),
-			$this->tokenTimeout,
-			'{}'
+			$volatile ? $this->tokenTimeoutVolatile : $this->tokenTimeoutLongterm,
+			$volatile ? json_encode([self::KEY_VOLATILE => 1]) : '{}'
 		);
 		CDECookieCache::getInstance()->write(
-			self::COOKIE_NAME_AUTH_TOKEN,
+			$this->cookieName,
 			$authenticationResponse->getAuthToken(),
-			$this->tokenTimeout
+			$volatile ? 0 : $this->tokenTimeoutLongterm
 		);
 		return $authenticationResponse->getAuthToken();
 	}
@@ -82,7 +113,7 @@ class AuthenticationProcessor {
 				$apiClient->userDeauthenticate($authToken);
 
 				//delete auth cookie
-				CDECookieCache::getInstance()->delete(self::COOKIE_NAME_AUTH_TOKEN);
+				CDECookieCache::getInstance()->delete($this->cookieName);
 			} catch (ObjectNotFoundException $e) {
 			}
 		} catch (AuthenticationRequiredException $e) {
@@ -99,11 +130,12 @@ class AuthenticationProcessor {
 	 * @throws AuthenticationRequiredException
 	 */
 	public function extendToken($authToken = null) {
+		// read from cookie
 		if (!$authToken) {
 			try {
-				$authToken = CDECookieCache::getInstance()->read(self::COOKIE_NAME_AUTH_TOKEN);
+				$authToken = CDECookieCache::getInstance()->read($this->cookieName);
 				if (empty($authToken)) {
-					CDECookieCache::getInstance()->delete(self::COOKIE_NAME_AUTH_TOKEN);
+					CDECookieCache::getInstance()->delete($this->cookieName);
 					throw new AuthenticationRequiredException();
 				}
 			} catch (CookieNotSetException $e) {
@@ -112,12 +144,17 @@ class AuthenticationProcessor {
 		}
 		$apiClient = new Client();
 		try {
-			$extendResponse = $apiClient->userUpdateToken($authToken, '{}', $this->requestApi->getRemoteAddress());
-			CDECookieCache::getInstance()->write(
-				self::COOKIE_NAME_AUTH_TOKEN,
-				$extendResponse->getAuthToken()->getToken(),
-				$this->tokenTimeout
-			);
+			// verify and extend the token
+			$extendResponse = $apiClient->userExtendToken($authToken, $this->requestApi->getRemoteAddress()->__toString());
+			$authToken = $extendResponse->getAuthToken()->getToken();
+			$metaInfo = json_decode($extendResponse->getAuthToken()->getMetaInfo(), true);
+
+			// update auth cookie if not volatile
+			if (!(isset($metaInfo[self::KEY_VOLATILE]) && $metaInfo[self::KEY_VOLATILE])) {
+				// TODO: retrieve expiry date from token?
+				CDECookieCache::getInstance()->write($this->cookieName, $authToken, $this->tokenTimeoutLongterm);
+			}
+
 			return $authToken;
 		} catch (ObjectNotFoundException $e) {
 			throw new AuthenticationRequiredException();
