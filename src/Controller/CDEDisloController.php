b10f76ebb4f74771177dcb8cc37fe299e515f62b
dschobert <d.schobert@ixolit.com>
2017-05-15 11:31:08 +0200

    added user context to cde dislo controller

--- src/Controller/CDEDisloController.php
+++ src/Controller/CDEDisloController.php
@@ -4,10 +4,14 @@ namespace Ixolit\Dislo\CDE\Controller;
 
 
 use Ixolit\CDE\Controller\CDEController;
+use Ixolit\CDE\Interfaces\FormProcessorInterface;
+use Ixolit\CDE\Interfaces\RequestAPI;
+use Ixolit\CDE\Interfaces\ResponseAPI;
 use Ixolit\Dislo\CDE\Auth\AuthenticationProcessor;
 use Ixolit\Dislo\CDE\Auth\AuthenticationRequiredException;
-use Ixolit\Dislo\CDE\CDEDisloClient;
 use Ixolit\Dislo\Client;
+use Ixolit\Dislo\Context\UserContext;
+use Ixolit\Dislo\WorkingObjects\User;
 
 /**
  * Class CDEDisloController
@@ -25,15 +29,31 @@ class CDEDisloController extends CDEController {
     /** @var string */
     private $authToken;
 
+    /** @var UserContext */
+    private $authenticatedUserContext;
+
+    /**
+     * CDEDisloController constructor.
+     *
+     * @param Client                      $client
+     * @param FormProcessorInterface|null $formProcessor
+     * @param RequestAPI|null             $requestApi
+     * @param ResponseAPI|null            $responseApi
+     */
+    public function __construct(Client $client,
+                                FormProcessorInterface $formProcessor = null,
+                                RequestAPI $requestApi = null,
+                                ResponseAPI $responseApi = null
+    ) {
+        parent::__construct($formProcessor, $requestApi, $responseApi);
+
+        $this->client = $client;
+    }
+
     /**
      * @return Client
      */
     protected function getClient() {
-        if (!isset($this->client)) {
-            //default client
-            $this->client = new CDEDisloClient();
-        }
-
         return $this->client;
     }
 
@@ -63,6 +83,27 @@ class CDEDisloController extends CDEController {
         }
 
         return $this->authToken;
+    }
+
+    /**
+     * @return UserContext
+     */
+    protected function getUserContext() {
+        if (!isset($this->authenticatedUserContext)) {
+            $this->authenticatedUserContext = new UserContext(
+                $this->getClient(),
+                $this->getAuthenticationProcessor()->getAuthenticatedUser()
+            );
+        }
+
+        return $this->authenticatedUserContext;
+    }
+
+    /**
+     * @return User
+     */
+    protected function getUser() {
+        return $this->getUserContext()->getUser();
     }
 
 }
