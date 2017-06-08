3c0059a0d7712d3ae9161e2bc41fb1ac0bd23640
Markus Marchewa <m.marchewa@ixolit.com>
2017-06-08 09:22:30 +0200

    added CDEInit::renderPreviewInfo

--- src/CDEInit.php
+++ src/CDEInit.php
@@ -3,10 +3,13 @@
 namespace Ixolit\Dislo\CDE;
 
 class CDEInit extends \Ixolit\CDE\CDEInit {
+
 	public static function execute() {
+
+		// TODO: get rid of this really ugly global!
 		global $cdeToolbar;
 
-		$cdeToolbar = new \Ixolit\Dislo\CDE\CDEToolbar(
+		$cdeToolbar = new CDEToolbar(
 			CDE::getRequestAPI(),
 			CDE::getResponseAPI(),
 			CDE::getFilesystemAPI(),
@@ -16,5 +19,14 @@ class CDEInit extends \Ixolit\CDE\CDEInit {
 		);
 
 		parent::execute();
+	}
+
+	public static function renderPreviewInfo() {
+
+		global $cdeToolbar;
+
+		if ($cdeToolbar instanceof CDEToolbar) {
+			$cdeToolbar->renderToolbar();
+		}
 	}
 }
