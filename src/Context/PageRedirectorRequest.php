a21d6bba7936c8d99e02b2f6caa7e918fc6467c9
Markus Marchewa <m.marchewa@ixolit.com>
2017-11-02 11:05:09 +0100

    added PageRedirectorRequest

---
+++ src/Context/PageRedirectorRequest.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Ixolit\Dislo\CDE\Context;
+
+use Ixolit\CDE\Context\Page;
+use Ixolit\CDE\Exceptions\GeoLookupFailedException;
+use Ixolit\Dislo\Redirector\Base\Cookie;
+use Ixolit\Dislo\Redirector\Base\RedirectorRequestInterface;
+use Ixolit\Dislo\Redirector\Base\RequestParameter;
+
+class PageRedirectorRequest implements RedirectorRequestInterface {
+
+	/** @var Page */
+	private $page;
+
+	/**
+	 * @param Page $page
+	 */
+	public function __construct(Page $page) {
+		$this->page = $page;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getScheme() {
+		return $this->page->getScheme();
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getHost() {
+		return $this->page->getVhost();
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getPath() {
+		return $this->page->getFullPath();
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getRequestParameters() {
+		$parameters = [];
+		foreach ($this->page->getRequestAPI()->getRequestParameters() as $name => $value) {
+			$parameters[] = (new RequestParameter())
+				->setName($name)
+				->setValue($value);
+		}
+		return $parameters;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getMethod() {
+		// TODO: implement in CDE
+		return 'GET';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getCookies() {
+		$cookies = [];
+		foreach ($this->page->getRequestAPI()->getCookies() as $cookie) {
+			$cookies[] = (new Cookie())
+				->setName($cookie->getName())
+				->setValue($cookie->getValue());
+		}
+		return $cookies;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getQuery() {
+		return $this->page->getQueryString();
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getHeaders() {
+		return $this->page->getRequestAPI()->getHeaders();
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getUserAgent() {
+		return $this->page->getRequestAPI()->getHeader('User-Agent');
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getIpBasedCountryCode() {
+		try {
+			$address = $this->page->getRequestAPI()->getRemoteAddress()->getAddress();
+			$lookup = $this->page->getGeoLookupApi()->lookup($address);
+			return $lookup->getCountry()->getIsoCode();
+		}
+		catch (GeoLookupFailedException $e) {
+			return null;
+		}
+	}
+}
