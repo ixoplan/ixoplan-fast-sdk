d5c3f5b28f55bbf88a671226d161e69caae7717b
Daniel Schobert <d.schobert@ixolit.com>
2021-03-31 06:44:30 +0000

    Added packageIdentifiers and onlyAvailable parameter to packagesList client method

--- src/CDEDisloClient.php
+++ src/CDEDisloClient.php
@@ -10,6 +10,7 @@ use Ixolit\Dislo\Request\RequestClient;
 use Ixolit\Dislo\Response\MiscGetRedirectorConfigurationResponse;
 use Ixolit\Dislo\Response\PackageGetResponse;
 use Ixolit\Dislo\Response\PackagesListResponse;
+use Ixolit\Dislo\WorkingObjects\Package;
 
 class CDEDisloClient extends Client {
 
@@ -38,7 +39,7 @@ class CDEDisloClient extends Client {
 		$packageIdentifier,
 		$cached = true
 	) {
-		$packages = $this->packagesList(null, $cached)->getPackages();
+		$packages = $this->packagesList(null, $cached, [$packageIdentifier])->getPackages();
 		foreach ($packages as $package) {
 			if ($package->getPackageIdentifier() == $packageIdentifier) {
 				return new PackageGetResponse($package);
@@ -47,33 +48,108 @@ class CDEDisloClient extends Client {
 		throw new ObjectNotFoundException('package with ID ' . $packageIdentifier);
 	}
 
-	/**
-	 * Retrieve a list of all packages registered in the system. This version uses the CDE key-value store to make
-	 * package list retrieval faster.
-	 *
-	 * @param null|string $serviceIdentifier
-	 * @param bool        $cached Use key-value store cached packages list instead of fetching it from the API.
-	 *
-	 * @return \Ixolit\Dislo\Response\PackagesListResponse
-	 */
+    /**
+     * Retrieve a list of all packages registered in the system. This version uses the CDE key-value store to make
+     * package list retrieval faster.
+     *
+     * @param null|string $serviceIdentifier
+     * @param bool        $cached Use key-value store cached packages list instead of fetching it from the API.
+     * @param array       $packageIdentifiers
+     * @param bool        $onlyAvailable
+     *
+     * @return \Ixolit\Dislo\Response\PackagesListResponse
+     */
 	public function packagesList(
 		$serviceIdentifier = null,
-		$cached = false
+		$cached = false,
+        array $packageIdentifiers = [],
+        $onlyAvailable = false
 	) {
 		if (!$cached || $this->getDevMode()) {
-			return parent::packagesList($serviceIdentifier);
+			return parent::packagesList($serviceIdentifier, $packageIdentifiers, $onlyAvailable);
 		} else {
 			try {
-				return PackagesListResponse::fromResponse(
-					[
-						'packages' => CDE::getKVSAPI()->get('apiPackages')
-					]
-				);
+			    return new PackagesListResponse($this->getPackagesFromKVS($serviceIdentifier, $packageIdentifiers, $onlyAvailable));
 			} catch (KVSKeyNotFoundException $e) {
-				return parent::packagesList($serviceIdentifier);
+				return parent::packagesList($serviceIdentifier, $packageIdentifiers, $onlyAvailable);
 			}
 		}
 	}
+
+    /**
+     * @param int|null $serviceIdentifier
+     * @param string[] $packageIdentifiers
+     * @param bool     $onlyAvailable
+     *
+     * @return Package[]
+     *
+     * @throws KVSKeyNotFoundException
+     */
+	private function getPackagesFromKVS($serviceIdentifier = null, array $packageIdentifiers = [], $onlyAvailable = false)
+    {
+        $packages = PackagesListResponse::fromResponse(
+            [
+                'packages' => CDE::getKVSAPI()->get('apiPackages')
+            ]
+        )->getPackages();
+
+        if ($serviceIdentifier) {
+            $packages = $this->filterPackagesByServiceIdentifier($packages, $serviceIdentifier);
+        }
+        if (!empty($packageIdentifiers)) {
+            $packages = $this->filterPackagesByIdentifiers($packages, $packageIdentifiers);
+        }
+        if ($onlyAvailable) {
+            $packages = $this->filterAvailablePlans($packages);
+        }
+
+        return $packages;
+    }
+
+    /**
+     * @param array $packages
+     * @param int   $serviceIdentifier
+     */
+    private function filterPackagesByServiceIdentifier(array $packages, $serviceIdentifier)
+    {
+        return \array_filter(
+            $packages,
+            function (Package $package) use ($serviceIdentifier) {
+                return $package->getServiceIdentifier() == $serviceIdentifier;
+            }
+        );
+    }
+
+    /**
+     * @param Package[] $packages
+     * @param string[]  $packageIdentifiers
+     *
+     * @return Package[]
+     */
+	private function filterPackagesByIdentifiers(array $packages, array $packageIdentifiers)
+    {
+        return \array_filter(
+            $packages,
+            function (Package $package) use ($packageIdentifiers) {
+                return \in_array($package->getPackageIdentifier(), $packageIdentifiers);
+            }
+        );
+    }
+
+    /**
+     * @param Package[] $packages
+     *
+     * @return Package[]
+     */
+	private function filterAvailablePlans(array $packages)
+    {
+        return \array_filter(
+            $packages,
+            function (Package $package) {
+                return $package->isSignupAvailable();
+            }
+        );
+    }
 
 	/**
 	 * Retrieve Dislo's redirector configuration. This version uses the CDE key-value store to make retrieval faster.
